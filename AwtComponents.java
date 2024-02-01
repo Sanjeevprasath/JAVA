43d19b2ee6733ad888c318770e11e83af70a108f
Sanjeevprasath <ssanjeevprasath@gmail.com>
2024-02-01 22:08:50 +0530

    file added

--- AwtComponents.java
+++ AwtComponents.java
@@ -1,8 +1,10 @@
 import java.awt.*;
 public class AwtComponents extends Frame{
     AwtComponents(){
+        TextField tf=new TextField("ENTER HERE");
+        tf.setBounds(200,350,150,100);
         Choice ch=new Choice();
-        c.setBounds(200,250,50,80);
+        ch.setBounds(200,250,50,80);
         Checkbox c=new Checkbox("Choice",true);
         c.setBounds(150,150,50,50);
         Button b=new Button("SUBMIT");
@@ -12,6 +14,7 @@ public class AwtComponents extends Frame{
         ch.add("100");
         ch.add("1000");
         add(ch);
+        add(tf);
         add(b);
         add(c);
         setSize(500,500);
